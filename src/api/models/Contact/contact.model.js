b5673078770bca0448f784441cb0539b396bbed4
amr <amr.hedeiwy@gmail.com>
2023-11-16 20:18:32 +0200

    fix added

--- src/api/models/Contact/contact.model.js
+++ src/api/models/Contact/contact.model.js
@@ -1,33 +1,17 @@
 import { Model } from 'sequelize';
 
 /**
-<<<<<<< HEAD
- * Defines the Contact model.
-=======
- * Defines the Conversation model.
->>>>>>> 9201da6286df4449e64f1b919576c15804459b9f
- *
  * @param {import('sequelize').Sequelize} sequelize - The Sequelize instance.
  * @param {import('sequelize').DataTypes} DataTypes - The data types module.
  */
 export default (sequelize, DataTypes) => {
   /**
-<<<<<<< HEAD
    * @class Contact
    *
    * @property {string} ContactID - The unique ID of the contact.
    * @property {string} Username - The contact's username.
    * @property {string} Email - The contact's email.
    * @property {string} Image - The contact's profile image (optional).
-=======
-   * @class Conversation
-   *
-   * @property {uuid} ConversationID - The unique ID of the conversation.
-   * @property {string} CreatedAt - The date when the conversation was created.
-   * @property {string} LastMessageAt - The date when the last message was sent.
-   * @property {string} Name - The name of the convesation (optional).
-   * @property {string} IsGroup - If the conversation is a group or not (optional).
->>>>>>> 9201da6286df4449e64f1b919576c15804459b9f
    */
   class Contact extends Model {}
 
@@ -44,12 +28,8 @@ export default (sequelize, DataTypes) => {
         type: DataTypes.STRING
       },
       Image: {
-<<<<<<< HEAD
         type: DataTypes.STRING,
         allowNull: true
-=======
-        type: DataTypes.STRING
->>>>>>> 9201da6286df4449e64f1b919576c15804459b9f
       }
     },
     {

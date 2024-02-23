f92c0dd4b66a8450bcc3e59859ec47290ef6f6fe
amr <amr.hedeiwy@gmail.com>
2024-02-23 14:36:37 +0200

    test6

--- src/lib/redis-client.js
+++ src/lib/redis-client.js
@@ -1,15 +1,9 @@
-import Redis from 'ioredis';
+import { createClient } from 'redis';
 
-// Initialize the Redis client.
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PASSWORD,
-  port: process.env.REDIS_PORT
+const redisClient = createClient({
+  url: process.env.REDIS_URL
 });
 
-// Attempt to connect to the Redis client.
-
-const redisClient = redis;
+await redisClient.connect().catch(console.error);
 
 export { redisClient };
